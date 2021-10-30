14dedb054c5c364ab9bf931eca354210dc958def
Alamin934 <islamalamin934@gmail.com>
2021-10-30 11:20:14 +0600

    find by email

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const ObjectId = require('mongodb').ObjectId;
 
 
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wpmdo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -19,12 +20,52 @@ app.get('/', (req, res) => {
 async function run() {
     try {
         await client.connect();
-        const database = client.db("Travel_Booking");
-        const servicesCollection = database.collection("Services");
-        // create a document to insert
+        const database = client.db("travel_booking");
+        const tourPlansCollection = database.collection("tour_plans");
+        const userPlansCollection = database.collection("user_plans");
+
+
+        //All Plans Get API
+        app.get('/allPlans', async (req, res) => {
+            const cursor = tourPlansCollection.find({});
+            const allPlans = await cursor.toArray();
+            res.send(allPlans);
+        });
+        // Post API
+        app.post('/addTourPlans', async (req, res) => {
+            const plan = req.body;
+            const result = await tourPlansCollection.insertOne(plan);
+            res.json(result);
+        });
+        //Get Single Plan id API
+        app.get('/allPlans/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await tourPlansCollection.findOne(query);
+            res.json(result);
+        });
+        //All User Plans Get Api
+        app.get('/userPlans', async (req, res) => {
+            const cursor = userPlansCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+        //User Plans Post API
+        app.post('/userPlans', async (req, res) => {
+            const userPlan = req.body;
+            const result = await userPlansCollection.insertOne(userPlan);
+            res.json(result);
+        });
+        //Get single User Plans By email
+        app.get('/myPlans/:email', async (req, res) => {
+            const email = req.params.email;
+            const cursor = userPlansCollection.find({ email });
+            const result = await cursor.toArray();
+            res.json(result);
+        });
 
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
